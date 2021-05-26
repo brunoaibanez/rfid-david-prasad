55f6deac23f54347892f65fb36fce1b477e0d405
Bruno <bruno.ibanezlopez@gmail.com>
2021-05-26 11:49:16 +0200

    First commit

---
+++ charlie.py
@@ -0,0 +1,107 @@
+import numpy as np
+import random
+import logging
+import logging.config
+import statistics
+import matplotlib.pyplot as plt
+class Charlie:
+    def __init__(self, id, k1, k2, loop):
+        self.logger = logging.getLogger(__name__)
+        self.logger.info('[Init] - Initializing Charlie')
+        self.L = 32
+        self.id = id
+        self.k1 = k1
+        self.k2 = k2
+        self.loop = loop
+
+        self.k1_list = [[] for i in range(self.L)]
+        self.k1_estimation = 0
+        self.k1_estimation_list = []
+
+        self.k2_list = [[] for i in range(self.L)]
+        self.k2_estimation = 0
+        self.k2_estimation_list = []
+        self.id_list = [[] for i in range(self.L)]
+        self.id_estimation = 0
+        
+    def receivesABD(self, a, b, d):
+        self.a = a
+        self.b = b
+        self.d = d
+    
+    def receivesEF(self, e, f):
+        self.e = e
+        self.f = f
+
+    def computeAproximation(self, l):
+        self.k1Estimation(l)
+        self.k2Estimation(l)
+        self.idEstimation(l)
+
+    
+    def k1Estimation(self,l):
+        output = "K1 estimation:"
+        self.k1_estimation = 0 
+        operations = [int(self.d), int(self.f), int(self.a ^ self.d), int(self.a ^ self.b ^ self.f), int(np.uint32(~(self.b ^ self.d))), int(self.b ^ self.f), int(self.a ^ self.b ^ self.d)]
+        for i in range(self.L):
+            for j in range(len(operations)):
+                aux = int(operations[j]/int(2**i))
+                value = aux % 2
+                self.k1_list[i].append(value)
+                
+            self.k1_estimation += int(statistics.median(self.k1_list[i])) * 2**i
+            
+        self.k1_estimation_list.append(self.k1_estimation)
+        if (self.loop-1) ==  l:
+            print("K1 value:     ", f'{self.k1:032b}')
+            print(output, f'{self.k1_estimation:032b}')
+
+    def k2Estimation(self, l):
+        output = "K2 estimation:"
+        self.k2_estimation = 0 
+        operations = [int(self.d), int(self.f), int(np.uint32(~(self.a ^ self.d))), int(self.a ^ self.f),  int(self.b ^ self.d),int(np.uint32(~(self.b ^ self.f))), int(self.a ^ self.b ^ self.f), int(self.a ^ self.b ^ self.d)]
+        for i in range(self.L):
+            for j in range(len(operations)):
+                aux = int(operations[j]/int(2**i))
+                value = aux % 2
+                self.k2_list[i].append(value)
+            self.k2_estimation += int(statistics.median(self.k2_list[i])) * 2**i
+        
+        self.k2_estimation_list.append(self.k2_estimation)
+        if (self.loop-1) ==  l:
+            print("K2 value:     ", f'{self.k2:032b}')
+            print(output, f'{self.k2_estimation:032b}')
+            
+
+    def hammingDistance(self,a,b):
+        res = 0
+        for i in range(self.L):
+            aux = int((a ^ b )/int(2**i))
+            value = aux % 2
+            res += value
+            
+        return res
+    
+    def printPlots(self):
+        k1_distances = []
+        for i in range(self.loop):
+            k1_distances.append(self.hammingDistance(self.k1, self.k1_estimation_list[i]))
+        plt.plot(range(self.loop),k1_distances)
+        plt.show()
+
+
+    def idEstimation(self, l):
+        output = "ID estimation:"
+        self.id_estimation = 0 
+        operations = [int(np.uint32(~(self.e ^ self.f))), int(self.a ^ self.b ^ self.e),  int(self.a ^ self.d ^ self.e), int(self.a ^ self.e ^ self.f),
+        int(self.b ^ self.d ^ self.e), int(self.d ^ self.e ^ self.f), int(np.uint32(~(self.a ^ self.b ^ self.d ^ self.e))),
+        int(self.a ^ self.d ^ self.e ^ self.f), int(np.uint32(~(self.b ^ self.d ^ self.e ^ self.f)))]
+        for i in range(self.L):
+            for j in range(len(operations)):
+                aux = int(operations[j]/int(2**i))
+                value = aux % 2
+                self.id_list[i].append(value)
+            self.id_estimation += int(statistics.median(self.id_list[i])) * 2**i 
+        if (self.loop-1) ==  l:
+            print("ID value:     ", f'{self.id:032b}')
+            print(output, f'{self.id_estimation:032b}')
---
+++ main.py
@@ -0,0 +1,62 @@
+from reader import Reader
+from tag import Tag
+from charlie import Charlie
+import logging
+import logging.config
+import random
+import sys
+
+class World:
+    
+    def __init__(self, loop): 
+        pid = random.randint(0, (pow(2,32)))
+        pid2 = random.randint(0, (pow(2,32)))
+        k1 = random.randint(0, (pow(2,32)))
+        k2 = random.randint(0, (pow(2,32)))
+        identificator = random.randint(0, pow(2,32))
+        self.loop = loop
+
+        self.reader = Reader(pid,pid2,k1,k2) # Reciben lo mismo tag y reader al init (self,pid, pid2, k1, k2)
+        self.tag = Tag(identificator,pid,pid2,k1,k2)  #(self,id, pid, pid2, k1, k2)
+        self.charlie = Charlie(identificator, k1, k2, self.loop)
+        #to do , create loop to simulate the rounds
+        # Reader starts with a,b,d
+    def start_simulation(self):
+        i = 0
+        while i < self.loop:
+            self.a = self.reader.generateA()
+            self.b = self.reader.generateB()
+            self.d = self.reader.generateD()
+            # Reader -> Charlie
+            self.charlie.receivesABD(self.a,self.b,self.d)
+            # Reader -> Tag
+            self.tag.receivesABD(self.a,self.b,self.d) # receibes A, B, D in order to get n1 and n2 and then check it
+            self.tag.calculateN1N2() # Calculate n1 and n2 with A, B
+            self.tag.checkN1N2() # Check result with D
+            self.e = self.tag.generateE()
+            self.f = self.tag.generateF()
+            # Tag -> Charlie
+            self.charlie.receivesEF(self.e,self.f)
+            # Tag -> Reader
+            self.reader.receivesEF(self.e,self.f)
+            self.reader.getID() # Calculate ID with E
+            self.reader.checkN1N2() # Check with F
+            # Round finished, now Charlie tries to guess k1, k2, ID
+            self.charlie.computeAproximation(i)
+            # Round finished, recalculating pseudonim pid and pid2
+            self.reader.recalculatePseudonim()
+            self.reader.updateN1N2()
+            self.tag.recalculatePseudonim()
+            i+=1
+        
+        self.charlie.printPlots()
+
+        
+          
+
+if __name__ == "__main__":
+    logging.config.fileConfig('logging.conf')
+    logger = logging.getLogger(__name__)
+    world = World(96)
+    world.start_simulation()
+ 
